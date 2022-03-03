6ab6eb4e56b1dfe09cac14bdffb063905e5d619d
Bogdan Zaharia <bogdan.zaharia@hootsuite.com>
2022-03-03 19:38:51 +0200

    add sequence fs action

--- packages/main/fs-driver/fsDriver.ts
+++ packages/main/fs-driver/fsDriver.ts
@@ -1,18 +1,20 @@
+import { FsClient, FsError } from '#interfaces/fsClient.api';
 import { T, Task } from '#ts-belt-extra';
-import { FilePath, FsClient, FsError } from '#interfaces/fsClient.api';
 import { A, pipe } from '@mobily/ts-belt';
 import { FsAction } from './types';
 
 export function initFsDriver(fs: FsClient) {
   return {
     perform: performAction,
-    performSequence(actions: FsAction[]): Task<readonly FilePath[], FsError> {
-      return pipe(actions, A.map(performAction), T.sequence);
+    performInParallel(actions: FsAction[]): Task<unknown, FsError> {
+      return pipe(actions, A.map(performAction), T.all);
     },
   };
 
-  function performAction(action: FsAction): Task<FilePath, FsError> {
+  function performAction(action: FsAction): Task<unknown, FsError> {
     switch (action.type) {
+      case 'sequence':
+        return pipe(action.actions, A.map(performAction), T.sequence);
       case 'writeFile':
         return fs.writeFile(action.path, action.contents);
       case 'symlink':
--- packages/main/fs-driver/types.ts
+++ packages/main/fs-driver/types.ts
@@ -2,4 +2,5 @@ import { FilePath } from '#interfaces/fsClient.api';
 
 export type FsAction =
   | { type: 'writeFile'; path: FilePath; contents: string }
-  | { type: 'symlink'; target: FilePath; path: FilePath };
+  | { type: 'symlink'; target: FilePath; path: FilePath }
+  | { type: 'sequence'; actions: FsAction[] };
